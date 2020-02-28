37859db583132ec7705e2d1f8cd518579371c049
hoidal <sjhoidal@gmail.com>
2020-02-28 15:16:44 -0700

    Initial Commit

---
+++ HikeTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HikeTracker
+//
+//  Created by Sam Hoidal on 2/28/20.
+//  Copyright © 2020 Sam Hoidal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
