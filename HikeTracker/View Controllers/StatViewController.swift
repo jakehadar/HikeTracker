04b75fdebb0cfc5c947e9f5488d3153a6bf6658d
hoidal <sjhoidal@gmail.com>
2020-03-04 14:00:33 -0700

    hike stats working

--- HikeTracker/View Controllers/StatViewController.swift
+++ HikeTracker/View Controllers/StatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 import CoreData
 
@@ -15,6 +16,7 @@ class StatViewController: UIViewController {
     @IBOutlet weak var distanceTravelledLabel: UILabel!
     @IBOutlet weak var averagePaceLabel: UILabel!
     @IBOutlet weak var netAltitudeLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     var timeElapsed = 0
     var distanceTravelled = Measurement(value: 0, unit: UnitLength.meters)
@@ -41,6 +43,54 @@ class StatViewController: UIViewController {
     }
     
     func loadMap() {
+        let region = mapRegion()
         
+        mapView.delegate = self
+        mapView.setRegion(region!, animated: true)
+        renderPolyline()
+    }
+    
+    func mapRegion() -> MKCoordinateRegion? {
+        let latitudes = polylineCoordinates.map { location -> Double in
+            return location.coordinate.latitude
+        }
+        
+        let longitudes = polylineCoordinates.map { location -> Double in
+            return location.coordinate.longitude
+        }
+        
+        let maxLat = latitudes.max()!
+        let minLat = latitudes.min()!
+        let maxLon = longitudes.max()!
+        let minLon = longitudes.min()!
+
+        let center = CLLocationCoordinate2D(latitude: (minLat + maxLat) / 2,
+                                          longitude: (minLon + maxLon) / 2)
+        let span = MKCoordinateSpan(latitudeDelta: (maxLat - minLat) * 1.3,
+                                  longitudeDelta: (maxLon - minLon) * 1.3)
+        return MKCoordinateRegion(center: center, span: span)
+    }
+    
+    func renderPolyline() {
+        let hikeCoordinates = polylineCoordinates.map { location -> CLLocationCoordinate2D in
+            return location.coordinate
+        }
+        
+        let hikePolyline = MKPolyline(coordinates: hikeCoordinates, count: hikeCoordinates.count)
+        
+        mapView.addOverlay(hikePolyline)
+    }
+    
+}
+
+extension StatViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let polyline = overlay as? MKPolyline else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+        let lineRenderer = MKPolylineRenderer(polyline: polyline)
+        lineRenderer.strokeColor = .systemBlue
+        lineRenderer.lineWidth = 3
+        return lineRenderer
     }
 }
