bc505ab57ae130ab073b8d121ccebb92b3b3af28
hoidal <sjhoidal@gmail.com>
2020-03-04 23:40:15 -0700

    final commit of night. coredata almost working

--- HikeTracker/View Controllers/StatViewController.swift
+++ HikeTracker/View Controllers/StatViewController.swift
@@ -30,6 +30,15 @@ class StatViewController: UIViewController {
     var altitudeLoss = 0
     var hikeComplete = false
     
+    // CoreData formatting
+    var hikeName = ""
+    var duration = Int16()
+    var distance = Double()
+    var elevationGain = Int16()
+    var elevationLoss = Int16()
+    var date = Date()
+    var locationList: [CLLocation] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +52,8 @@ class StatViewController: UIViewController {
         let formattedDistance = FormatDisplay.distance(distanceTravelled)
         let formattedTime = FormatDisplay.time(timeElapsed)
         let formattedPace = FormatDisplay.pace(distance: distanceTravelled, seconds: timeElapsed, outputUnit: UnitSpeed.minutesPerMile)
+        
+        distance = (formattedDistance as NSString).doubleValue
                 
         timeElapsedLabel.text = "\(formattedTime)"
         distanceTravelledLabel.text = "\(formattedDistance)"
@@ -122,6 +133,79 @@ class StatViewController: UIViewController {
     }
     
     
+    @IBAction func saveHike(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Hike Name",
+                                      message: "Add a name to remember this hike",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) {
+            [unowned self] action in
+
+            guard let textField = alert.textFields?.first,
+            let nameToSave = textField.text else { return }
+            self.hikeName = nameToSave
+            self.formatData()
+        }
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    
+    @IBAction func discardHike(_ sender: Any) {
+        let alertController = UIAlertController(title: "Are you sure?",
+                                                message: "Do you wish to delete this hike?",
+                                                preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.addAction(UIAlertAction(title: "Delete Hike", style: .destructive) { _ in
+                 _ = self.navigationController?.popToRootViewController(animated: true)
+        })
+               
+        present(alertController, animated: true)
+    }
+    
+    func formatData() {
+        duration = Int16(timeElapsed)
+        elevationGain = Int16(altitudeGain)
+        elevationLoss = Int16(altitudeLoss)
+
+        saveHikeData()
+    }
+    
+    func saveHikeData() {
+    
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let hikeEntity = NSEntityDescription.entity(forEntityName: "Hike", in: managedContext)!
+      
+        let hikeData = NSManagedObject(entity: hikeEntity, insertInto: managedContext)
+        hikeData.setValue(hikeName, forKey: "name")
+        hikeData.setValue(distance, forKey: "distance")
+        hikeData.setValue(duration, forKey: "duration")
+        hikeData.setValue(elevationGain, forKey: "elevation_gain")
+        hikeData.setValue(elevationLoss, forKey: "elevation_loss")
+        hikeData.setValue(locationList, forKey: "locationList")
+        hikeData.setValue(date, forKey: "timestamp")
+
+        do {
+            try managedContext.save()
+           
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+
 }
 
 extension StatViewController: MKMapViewDelegate {
